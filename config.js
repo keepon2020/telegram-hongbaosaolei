938eae60eb516a49b5cc14a3d707db80dea270fa
keepon2020 <134042233+keepon2020@users.noreply.github.com>
2023-05-19 15:04:29 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+var url = "http://127.0.0.1:4771" //node.js的根路径地址,必填
+var projectName = "V1.0" //项目名称,必填
+var companyName = "网易(杭州)网络有限公司 2002-2023" //公司名称,非必填
+var companyWebUrl = "https://www.163.com/" //公司官网地址,非必填
+
+
+
+
+
+
+
+
+
